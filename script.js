a399483ff7a7468fabe316154741aa99c91b078c
Muhammad Basim <muhammadbasim27811@gmail.com>
2026-09-23 21:47:04 +0500

    file

---
+++ script.js
@@ -0,0 +1,49 @@
+function red() {
+    document.body.style.backgroundColor = "red";
+    document.body.style.color = "black";
+}
+
+function blue() {
+    document.body.style.backgroundColor = "blue";
+    document.body.style.color = "white";
+}
+
+function green() {
+    document.body.style.backgroundColor = "green";
+    document.body.style.color = "white";
+}
+
+function yellow() {
+    document.body.style.backgroundColor = "yellow";
+    document.body.style.color = "black";
+}
+
+function purple() {
+    document.body.style.backgroundColor = "purple";
+    document.body.style.color = "white";
+}
+
+function orange() {
+    document.body.style.backgroundColor = "orange";
+    document.body.style.color = "black";
+}
+
+function pink() {
+    document.body.style.backgroundColor = "pink";
+    document.body.style.color = "black";
+}
+
+function brown() {
+    document.body.style.backgroundColor = "brown";
+    document.body.style.color = "white";
+}
+
+function black() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function reset() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
